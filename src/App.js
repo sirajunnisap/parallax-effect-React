e86d12f0356ddf37d33b25b0e5a7e98d89501ea2
sirajunnisa <sirajunnisap@gmail.com>
2023-12-22 09:52:19 +0530

    Remove: Delete unnecessary files and code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 
-import './App.css';
 import ImageOne from './components/ImageOne';
 import ImageThree from './components/ImageThree';
 import ImageTwo from './components/ImageTwo';
-import TextBox from './components/TextBox';
 
 function App() {
   return (
